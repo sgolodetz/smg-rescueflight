fb69723a85c57c70e46913210e51e3c8b7a44c9f
Stuart Golodetz <sgolodetz@gxstudios.net>
2020-11-11 22:08:03 +0000

    Introduce threading to make things more responsive.

--- sandbox/try_drone_depth.py
+++ sandbox/try_drone_depth.py
@@ -1,6 +1,7 @@
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
+import threading
 
 from argparse import ArgumentParser
 from typing import Dict, Optional
@@ -8,6 +9,71 @@ from typing import Dict, Optional
 from smg.mvdepthnet.mvdepthestimator import MVDepthEstimator
 from smg.pyorbslam import MonocularTracker
 from smg.rotory.drone_factory import DroneFactory
+
+
+class DepthHandler:
+    def __init__(self):
+        self.__front_buffer: np.ndarray = np.zeros((256, 320), dtype=float)
+        self.__image: Optional[np.ndarray] = None
+        self.__pose: Optional[np.ndarray] = None
+        self.__reference_image: Optional[np.ndarray] = None
+        self.__reference_pose: Optional[np.ndarray] = None
+        self.__request_pending: bool = False
+        self.__should_terminate: bool = False
+
+        self.__get_lock = threading.Lock()
+        self.__request_lock = threading.Lock()
+
+        self.__no_pending_request = threading.Condition(self.__request_lock)
+        self.__pending_request = threading.Condition(self.__request_lock)
+
+        depth_thread = threading.Thread(target=self.__process_depth_requests)
+        depth_thread.start()
+
+    def get_depth(self):
+        with self.__get_lock:
+            return self.__front_buffer.copy()
+
+    def request_depth(self, image: np.ndarray, reference_image: np.ndarray, pose: np.ndarray, reference_pose: np.ndarray):
+        acquired: bool = self.__request_lock.acquire(blocking=False)
+        if acquired:
+            self.__image = image
+            self.__pose = pose
+            self.__reference_image = reference_image
+            self.__reference_pose = reference_pose
+            self.__request_pending = True
+            self.__pending_request.notify()
+            self.__request_lock.release()
+
+    def terminate(self):
+        self.__should_terminate = True
+
+    def __process_depth_requests(self):
+        model_path: str = "C:/Users/Stuart Golodetz/Downloads/MVDepthNet/opensource_model.pth.tar"
+        # TODO: Use calibrated intrinsics.
+        intrinsics: np.ndarray = np.array([
+            [921.0, 0.0, 480.0],
+            [0.0, 921.0, 360.0],
+            [0.0, 0.0, 1.0]
+        ])
+        depth_estimator: MVDepthEstimator = MVDepthEstimator(model_path, intrinsics)
+
+        with self.__request_lock:
+            while not self.__should_terminate:
+                while not self.__request_pending:
+                    self.__pending_request.wait(0.1)
+                    if self.__should_terminate:
+                        return
+
+                back_buffer: np.ndarray = depth_estimator.estimate_depth(
+                    self.__image, self.__reference_image, self.__pose, self.__reference_pose
+                )
+
+                with self.__get_lock:
+                    self.__front_buffer = back_buffer
+
+                self.__request_pending = False
+                self.__no_pending_request.notify()
 
 
 def main():
@@ -32,17 +98,12 @@ def main():
             settings_file=f"settings-{drone_type}.yaml", use_viewer=True,
             voc_file="C:/orbslam/Vocabulary/ORBvoc.txt", wait_till_ready=False
         ) as tracker:
-            model_path: str = "C:/Users/Stuart Golodetz/Downloads/MVDepthNet/opensource_model.pth.tar"
-            # TODO: Use calibrated intrinsics.
-            intrinsics: np.ndarray = np.array([
-                [921.0, 0.0, 480.0],
-                [0.0, 921.0, 360.0],
-                [0.0, 0.0, 1.0]
-            ])
-            depth_estimator: MVDepthEstimator = MVDepthEstimator(model_path, intrinsics)
-
             reference_image: Optional[np.ndarray] = None
             reference_pose: Optional[np.ndarray] = None
+
+            depth_handler: DepthHandler = DepthHandler()
+
+            _, ax = plt.subplots(1, 2)
 
             while True:
                 image: np.ndarray = drone.get_image()
@@ -60,12 +121,20 @@ def main():
                     reference_image = image
                     reference_pose = pose
                 else:
-                    depth_image: np.ndarray = depth_estimator.estimate_depth(
+                    depth_handler.request_depth(
                         image, reference_image, pose, reference_pose
                     )
-                    plt.imshow(depth_image)
+
+                depth_image: Optional[np.ndarray] = depth_handler.get_depth()
+                if depth_image is not None:
+                    ax[0].clear()
+                    ax[1].clear()
+                    ax[0].imshow(image)
+                    ax[1].imshow(depth_image)
                     plt.draw()
                     plt.waitforbuttonpress(0.001)
+
+            depth_handler.terminate()
 
 
 if __name__ == "__main__":
