2124196ecac7b4db0b2d077643b64009ee9e4b50
Stuart Golodetz <sgolodetz@gxstudios.net>
2021-02-17 21:59:42 +0000

    More tidying.

--- smg/rescueflight/comms/try_remote_skeleton_detector.py
+++ smg/rescueflight/comms/try_remote_skeleton_detector.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 
+from argparse import ArgumentParser
 from timeit import default_timer as timer
 from typing import List, Optional
 
@@ -9,21 +10,28 @@ from smg.skeletons import Skeleton
 
 
 def main() -> None:
+    # Parse any command-line arguments.
+    parser = ArgumentParser()
+    parser.add_argument(
+        "--input_file", "-i", type=str, default="D:/LCRNet_v2.0/skeleton.png", help="the input image"
+    )
+    args: dict = vars(parser.parse_args())
+
+    # Construct the remote skeleton detector.
     with RemoteSkeletonDetector() as skeleton_detector:
-        frame_idx: int = 0
-        # image: np.ndarray = cv2.imread("C:/smglib/smg-lcrnet/smg/external/lcrnet/058017637.jpg")
-        image: np.ndarray = cv2.imread("D:/LCRNet_v2.0/skeleton.png")
+        image: np.ndarray = cv2.imread(args["input_file"])
         world_from_camera: np.ndarray = np.eye(4)
+
+        # Repeatedly detect the people in the input image and print out some debug messages.
         while True:
             start = timer()
             skeletons: Optional[List[Skeleton]] = skeleton_detector.detect_skeletons(image, world_from_camera)
             end = timer()
 
             if skeletons is not None:
-                print(f"{frame_idx}: Remote Detection Time: {end - start}s")
-                print(f"{frame_idx}: {skeletons}")
-
-            frame_idx += 1
+                print(f"Remote Detection Time: {end - start}s")
+                print(f"Skeletons: {skeletons}")
+                print("===")
 
 
 if __name__ == "__main__":
