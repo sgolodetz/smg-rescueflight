b981ac3b8c832073d23ea73e4d1f248347b24098
Stuart Golodetz <sgolodetz@gxstudios.net>
2021-02-19 18:40:23 +0000

    Try out set_node_value and delete_node.

--- smg/rescueflight/pyoctomap/mine/try_insert_ray.py
+++ smg/rescueflight/pyoctomap/mine/try_insert_ray.py
@@ -13,10 +13,15 @@ def main() -> None:
     origin: Vector3 = Vector3(half_voxel_size, half_voxel_size, half_voxel_size)
     offset: Vector3 = Vector3(voxel_size * 10, 0.0, 0.0)
 
-    for angle in np.linspace(0.0, 2 * math.pi, 8, endpoint=False):
+    for angle in np.linspace(0.0, 2 * math.pi, 128, endpoint=False):
         angled_offset: Vector3 = offset.copy()
         angled_offset.rotate_ip(0, 0, angle)
         tree.insert_ray(origin, origin + angled_offset)
+
+    for x in np.linspace(0.0, voxel_size * 20, 20, endpoint=True):
+        # tree.update_node(Vector3(x, 0, 0), False)
+        # tree.set_node_value(Vector3(x, 0, 0), -np.inf)
+        tree.delete_node(Vector3(x, 0, 0))
 
     tree.write_binary("insert_ray.bt")
 
