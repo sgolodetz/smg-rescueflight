237c7921cbfb77f89f484e6309ca08694c724587
Stuart Golodetz <sgolodetz@gxstudios.net>
2021-02-21 23:48:18 +0000

    Get bounding shape rasterisation working.

--- smg/rescueflight/utility/try_shapes.py
+++ smg/rescueflight/utility/try_shapes.py
@@ -1,8 +1,13 @@
-from smg.utility import Cylinder, Sphere
+import numpy as np
+
+from typing import List
+
+from smg.utility import Cylinder, ShapeUtil, Sphere
 
 
 def main() -> None:
     sphere: Sphere = Sphere(centre=[0, 0, 0], radius=1.0)
+    print(sphere.mins(), sphere.maxs())
     print(sphere.classify_point([0.5, 0, 0]))
     print(sphere.classify_point([1, 0, 0]))
     print(sphere.classify_point([2, 0, 0]))
@@ -12,6 +17,7 @@ def main() -> None:
         base_centre=[0, 0, 0], base_radius=1.0,
         top_centre=[0, 0, 10], top_radius=2.0
     )
+    print(cylinder.mins(), cylinder.maxs())
     print(cylinder.classify_point([1, 0, 0]))
     print(cylinder.classify_point([1.49, 0, 5]))
     print(cylinder.classify_point([1.5, 0, 5]))
@@ -22,6 +28,15 @@ def main() -> None:
     print(cylinder.classify_point([0, 0, 9.9]))
     print(cylinder.classify_point([0, 0, 10]))
     print(cylinder.classify_point([0, 0, 10.1]))
+    print()
+
+    from pprint import pprint
+    from timeit import default_timer as timer
+    start = timer()
+    voxels: List[np.ndarray] = ShapeUtil.rasterise_shapes([sphere, cylinder], 5.0)
+    end = timer()
+    pprint(voxels)
+    print(f"Time: {end - start}s")
 
 
 if __name__ == "__main__":
