3611896fdab0e86d8c4f59fffa0282db040d1579
Stuart Golodetz <sgolodetz@gxstudios.net>
2021-05-04 13:58:42 +0100

    Add key tap gestures.

--- smg/rescueflight/pyleap/examples/try_gestures.py
+++ smg/rescueflight/pyleap/examples/try_gestures.py
@@ -8,6 +8,7 @@ def main() -> None:
 
     controller: LeapController = LeapController()
     controller.enable_gesture(leap.GT_CIRCLE)
+    controller.enable_gesture(leap.GT_KEY_TAP)
     controller.enable_gesture(leap.GT_SWIPE)
 
     while True:
@@ -15,6 +16,9 @@ def main() -> None:
         if frame.is_valid():
             for i in range(len(frame.gestures())):
                 gesture: leap.Gesture = frame.gestures()[i]
+                if not gesture.is_valid():
+                    continue
+
                 if gesture.type() == leap.GT_CIRCLE:
                     circle_gesture: leap.CircleGesture = leap.CircleGesture(gesture)
                     print(
@@ -22,6 +26,9 @@ def main() -> None:
                         f"{circle_gesture.centre()}, {circle_gesture.normal()}, {circle_gesture.radius()}, "
                         f"{circle_gesture.progress()}"
                     )
+                elif gesture.type() == leap.GT_KEY_TAP:
+                    key_tap_gesture: leap.KeyTapGesture = leap.KeyTapGesture(gesture)
+                    print(f"Key Tap Gesture ({gesture.id()}, {gesture.state()}): {key_tap_gesture.position()}")
                 elif gesture.type() == leap.GT_SWIPE:
                     swipe_gesture: leap.SwipeGesture = leap.SwipeGesture(gesture)
                     print(f"Swipe Gesture ({gesture.id()}, {gesture.state()}): {swipe_gesture.direction()}")
