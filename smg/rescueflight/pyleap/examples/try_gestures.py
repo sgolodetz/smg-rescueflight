a10761ddec3832b69eda5be71257cbf879341653
Stuart Golodetz <sgolodetz@gxstudios.net>
2021-05-04 12:20:02 +0100

    Add support for gesture states and circle gestures.

--- smg/rescueflight/pyleap/examples/try_gestures.py
+++ smg/rescueflight/pyleap/examples/try_gestures.py
@@ -7,6 +7,7 @@ def main() -> None:
     np.set_printoptions(suppress=True)
 
     controller: LeapController = LeapController()
+    controller.enable_gesture(leap.GT_CIRCLE)
     controller.enable_gesture(leap.GT_SWIPE)
 
     while True:
@@ -14,9 +15,15 @@ def main() -> None:
         if frame.is_valid():
             for i in range(len(frame.gestures())):
                 gesture: leap.Gesture = frame.gestures()[i]
-                if gesture.type() == leap.GT_SWIPE:
+                if gesture.type() == leap.GT_CIRCLE:
+                    circle_gesture: leap.CircleGesture = leap.CircleGesture(gesture)
+                    print(
+                        f"Circle Gesture ({gesture.id()}, {gesture.state()}): "
+                        f"{circle_gesture.centre()}, {circle_gesture.normal()}"
+                    )
+                elif gesture.type() == leap.GT_SWIPE:
                     swipe_gesture: leap.SwipeGesture = leap.SwipeGesture(gesture)
-                    print(f"Swipe Gesture: {swipe_gesture.direction()}")
+                    print(f"Swipe Gesture ({gesture.id()}, {gesture.state()}): {swipe_gesture.direction()}")
 
 
 if __name__ == "__main__":
