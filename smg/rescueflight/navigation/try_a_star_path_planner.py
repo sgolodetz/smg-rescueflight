82feae9c4078b3c7d1ea8156917ce01344b43801
Stuart Golodetz <sgolodetz@gxstudios.net>
2021-05-06 11:51:41 +0100

    Continue working on path clearance.

--- smg/rescueflight/navigation/try_a_star_path_planner.py
+++ smg/rescueflight/navigation/try_a_star_path_planner.py
@@ -42,7 +42,7 @@ def main() -> None:
     tree: OcTree = OcTree(voxel_size)
     tree.read_binary("C:/smglib/smg-mapping/output-navigation/octree.bt")
 
-    planner: AStarPathPlanner = AStarPathPlanner(tree, PathUtil.neighbours26)
+    planner: AStarPathPlanner = AStarPathPlanner(tree, PathUtil.neighbours6)
     source = np.array([0.5, -0.5, 0.5]) * voxel_size
     # goal = np.array([20.5, 0.5, 20.5]) * voxel_size
     goal = np.array([30.5, 5.5, 17.5]) * voxel_size
