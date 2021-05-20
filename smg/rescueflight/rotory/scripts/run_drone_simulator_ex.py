aec263caf7066d7d2ad4a30c31607a0fb6b1f5d7
Stuart Golodetz <sgolodetz@gxstudios.net>
2021-05-20 10:38:52 +0100

    Add in the command-line arguments.

--- smg/rescueflight/rotory/scripts/run_drone_simulator_ex.py
+++ smg/rescueflight/rotory/scripts/run_drone_simulator_ex.py
@@ -1,9 +1,26 @@
+from argparse import ArgumentParser
 from typing import Tuple
 
 from drone_simulator import DroneSimulator
 
 
 def main() -> None:
+    # Parse any command-line arguments.
+    parser = ArgumentParser()
+    parser.add_argument(
+        "--planning_octree", type=str,
+        help="the name of the planning octree file"
+    )
+    parser.add_argument(
+        "--scene_mesh", type=str,
+        help="the name of the scene mesh file"
+    )
+    parser.add_argument(
+        "--scene_octree", type=str,
+        help="the name of the scene octree file"
+    )
+    args: dict = vars(parser.parse_args())
+
     # Specify the camera intrinsics.
     intrinsics: Tuple[float, float, float, float] = (532.5694641250893, 531.5410880910171, 320.0, 240.0)
 
@@ -12,8 +29,8 @@ def main() -> None:
         drone_mesh_filename="C:/smglib/meshes/tello.ply",
         intrinsics=intrinsics,
         plan_paths=True,
-        scene_mesh_filename="C:/smglib/smg-mapping/output-navigation/mesh.ply",
-        scene_octree_filename="C:/smglib/smg-mapping/output-navigation/octree5cm.bt"
+        scene_mesh_filename=args.get("scene_mesh"),
+        scene_octree_filename=args.get("scene_octree")
     ) as simulator:
         # Run the simulator.
         simulator.run()
