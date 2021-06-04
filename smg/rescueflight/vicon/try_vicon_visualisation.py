378d74ef46eab8e76b1356d2604d2ce639b5b71d
Stuart Golodetz <sgolodetz@gxstudios.net>
2021-06-04 13:18:42 +0100

    Start aligning the reconstructed scene to the Vicon coordinate system.

--- smg/rescueflight/vicon/try_vicon_visualisation.py
+++ smg/rescueflight/vicon/try_vicon_visualisation.py
@@ -1,4 +1,5 @@
 import numpy as np
+import open3d as o3d
 import os
 
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
@@ -6,13 +7,70 @@ import pygame
 
 from OpenGL.GL import *
 from timeit import default_timer as timer
-from typing import Optional, Tuple
+from typing import Dict, Optional, Tuple
 
-from smg.opengl import CameraRenderer, OpenGLMatrixContext, OpenGLUtil
+from smg.opengl import CameraRenderer, OpenGLMatrixContext, OpenGLTriMesh, OpenGLUtil
 from smg.rigging.cameras import SimpleCamera
 from smg.rigging.controllers import KeyboardCameraController
 from smg.rigging.helpers import CameraPoseConverter, CameraUtil
+from smg.utility import FiducialUtil, GeometryUtil
 from smg.vicon import SubjectFromSourceCache, ViconInterface
+
+
+# FIXME: This is a temporary copy of the one from DroneSimulator - it should be put somewhere more central.
+def convert_trimesh_to_opengl(o3d_mesh: o3d.geometry.TriangleMesh) -> OpenGLTriMesh:
+    """
+    Convert an Open3D triangle mesh to an OpenGL one.
+
+    :param o3d_mesh:    The Open3D triangle mesh.
+    :return:            The OpenGL mesh.
+    """
+    # FIXME: This should probably be moved somewhere more central at some point.
+    o3d_mesh.compute_vertex_normals(True)
+    return OpenGLTriMesh(
+        np.asarray(o3d_mesh.vertices),
+        np.asarray(o3d_mesh.vertex_colors),
+        np.asarray(o3d_mesh.triangles),
+        vertex_normals=np.asarray(o3d_mesh.vertex_normals)
+    )
+
+
+def estimate_scene_transformation(vicon: ViconInterface) -> np.ndarray:
+    # Load in the positions of the four marker corners as estimated during the ground-truth reconstruction.
+    fiducials: Dict[str, np.ndarray] = FiducialUtil.load_fiducials(
+        "C:/spaint/build/bin/apps/spaintgui/meshes/TangoCapture-20210604-124834-fiducials.txt"
+    )
+
+    # Stack these positions into a 3x4 matrix.
+    p: np.ndarray = np.column_stack([
+        fiducials["0_0"],
+        fiducials["0_1"],
+        fiducials["0_2"],
+        fiducials["0_3"]
+    ])
+
+    # TODO
+    marker_positions: Dict[str, np.ndarray] = vicon.get_marker_positions("Registrar")
+
+    q: np.ndarray = np.column_stack([
+        marker_positions["0_0"],
+        marker_positions["0_1"],
+        marker_positions["0_2"],
+        marker_positions["0_3"]
+    ])
+
+    print(p)
+    print(q)
+
+    # Estimate and return the rigid transformation between the two sets of points.
+    transform: np.ndarray = GeometryUtil.estimate_rigid_transform(p, q)
+    print(transform)
+    # transform[0:3, 0:3] = np.array([
+    #     [1.0, 0.0, 0.0],
+    #     [0.0, 0.0, 1.0],
+    #     [0.0, -1.0, 0.0]
+    # ])
+    return transform
 
 
 def main() -> None:
@@ -41,6 +99,14 @@ def main() -> None:
 
     # Connect to the Vicon system.
     with ViconInterface() as vicon:
+        if vicon.get_frame():
+            # Load in the scene mesh (if any).
+            scene_mesh_o3d: o3d.geometry.TriangleMesh = o3d.io.read_triangle_mesh(
+                "C:/spaint/build/bin/apps/spaintgui/meshes/TangoCapture-20210604-124834-cleaned.ply"
+            )
+            scene_mesh_o3d.transform(estimate_scene_transformation(vicon))
+            scene_mesh: Optional[OpenGLTriMesh] = convert_trimesh_to_opengl(scene_mesh_o3d)
+
         # Repeatedly:
         while True:
             # Process any PyGame events.
@@ -77,6 +143,10 @@ def main() -> None:
                     # Render a voxel grid.
                     glColor3f(0.0, 0.0, 0.0)
                     OpenGLUtil.render_voxel_grid([-3, -5, 0], [3, 5, 2], [1, 1, 1], dotted=True)
+
+                    # Render the scene mesh (if any).
+                    if scene_mesh is not None:
+                        scene_mesh.render()
 
                     # If a frame of Vicon data is available:
                     if vicon.get_frame():
