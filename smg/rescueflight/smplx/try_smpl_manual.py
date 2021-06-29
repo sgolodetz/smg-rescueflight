10fed1378b9a307d250949cd192499d3e52a661c
Stuart Golodetz <sgolodetz@gxstudios.net>
2021-06-29 13:13:01 +0100

    Yet more tidying.

--- smg/rescueflight/smplx/try_smpl_manual.py
+++ smg/rescueflight/smplx/try_smpl_manual.py
@@ -18,6 +18,11 @@ from smg.smplx import SMPLBody
 
 
 def draw_frame(body: SMPLBody) -> None:
+    """
+    Draw the current frame.
+
+    :param body:    The SMPL body model.
+    """
     glClearColor(1.0, 1.0, 1.0, 1.0)
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
@@ -34,28 +39,36 @@ def draw_frame(body: SMPLBody) -> None:
 
 
 def main():
+    # Initialise PyGame and create the window.
     pygame.init()
     window_size = (640, 480)
     pygame.display.set_mode(window_size, pygame.DOUBLEBUF | pygame.OPENGL)
+    pygame.display.set_caption("SMPL Manual Pose Demo")
 
     # Load the SMPL body model.
     body: SMPLBody = SMPLBody("male")
 
+    # Enable the z-buffer.
     glEnable(GL_DEPTH_TEST)
     glDepthFunc(GL_LESS)
 
+    # Set the projection matrix.
     glMatrixMode(GL_PROJECTION)
     gluPerspective(45, (window_size[0] / window_size[1]), 0.1, 1000.0)
 
+    # Initialise the variables that control the animation.
     angle: float = 0.0
     direction: int = 0
 
+    # Repeatedly:
     while True:
+        # Process any PyGame events.
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit(0)
 
+        # Update the animation variables.
         if direction == 0:
             if angle < 60.0:
                 angle += 0.5
@@ -71,6 +84,7 @@ def main():
 
         start = timer()
 
+        # Manually set the local joint rotations based on the animation variables.
         body_pose: np.ndarray = np.zeros(smplx.SMPL.NUM_BODY_JOINTS * 3, dtype=np.float32)
         body_pose[3:6] = -np.array([1, 0, 0]) * angle * math.pi / 180
         body_pose[6:9] = np.array([1, 0, 0]) * angle * math.pi / 180
@@ -78,8 +92,9 @@ def main():
         body_pose[21:24] = np.array([1, 0, 0]) * angle / 2 * math.pi / 180
         body_pose[48:51] = -np.array([1, 0, 0]) * math.pi / 2
         body_pose[54:57] = np.array([0, 0, 1]) * math.pi / 2
-
         body.set_manual_pose(body_pose, np.eye(4))
+
+        # Draw the current frame.
         draw_frame(body)
 
         end = timer()
--- smg/rescueflight/smplx/try_smpl_skeleton.py
+++ smg/rescueflight/smplx/try_smpl_skeleton.py
@@ -37,7 +37,7 @@ def main() -> None:
     pygame.init()
     window_size: Tuple[int, int] = (640, 480)
     pygame.display.set_mode(window_size, pygame.DOUBLEBUF | pygame.OPENGL)
-    pygame.display.set_caption("SMPL Body Demo")
+    pygame.display.set_caption("SMPL Skeleton Demo")
 
     # Enable the z-buffer.
     glEnable(GL_DEPTH_TEST)
@@ -52,7 +52,6 @@ def main() -> None:
     body: SMPLBody = SMPLBody(
         "female",
         texture_coords_filename="D:/smplx/textures/smpl/texture_coords.npy",
-        # texture_image_filename="D:/smplx/textures/smpl/texture.png"
         texture_image_filename="D:/smplx/textures/smpl/surreal/nongrey_female_0891.jpg"
     )
 
@@ -67,20 +66,24 @@ def main() -> None:
             intrinsics: Tuple[float, float, float, float] = calib.get_intrinsics("colour")
             image_size: Tuple[int, int] = calib.get_image_size("colour")
 
+            # Initialise a few variables.
             colour_image: Optional[np.ndarray] = None
             frame_idx: int = 0
             pause: bool = True
             process_next: bool = True
             skeletons: List[Skeleton3D] = []
 
-            # TODO
+            # Repeatedly:
             while True:
                 # Process any PyGame events.
                 for event in pygame.event.get():
                     if event.type == pygame.KEYDOWN:
+                        # If the user presses the 'b' key, process the sequence without pausing.
                         if event.key == pygame.K_b:
                             pause = False
                             process_next = True
+
+                        # Otherwise, if the user presses the 'n' key, process the next image and then pause.
                         elif event.key == pygame.K_n:
                             pause = True
                             process_next = True
@@ -95,7 +98,7 @@ def main() -> None:
                 # Try to load an RGB-D frame from disk.
                 frame: Optional[Dict[str, Any]] = RGBDSequenceUtil.try_load_frame(frame_idx, sequence_dir)
 
-                # TODO
+                # If the frame was successfully loaded and we're processing it now:
                 if frame is not None and process_next:
                     colour_image = frame["colour_image"]
 
@@ -108,6 +111,7 @@ def main() -> None:
                     # Advance to the next frame.
                     frame_idx += 1
 
+                    # Decide whether to continue processing subsequent frames or wait.
                     process_next = not pause
 
                 # Show the most recent colour image (if any) so that the user can see what's going on.
@@ -155,6 +159,7 @@ def main() -> None:
                 # Swap the front and back buffers.
                 pygame.display.flip()
     except RuntimeError as e:
+        # If any exception is raised, print it out so that we can see what happened.
         print(e)
 
 
