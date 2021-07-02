1a004ceebf80a5d0496e3242a88ae555d26aa84a
Stuart Golodetz <sgolodetz@gxstudios.net>
2021-07-02 12:37:32 +0100

    Try persisting Vicon skeletons to disk and improve the skeleton detector.

--- smg/rescueflight/vicon/try_vicon_interface.py
+++ smg/rescueflight/vicon/try_vicon_interface.py
@@ -11,13 +11,14 @@ def main() -> None:
             if vicon.get_frame():
                 print(f"=== Frame {vicon.get_frame_number()} ===")
 
-                for name, pos in vicon.get_marker_positions("Wand").items():
-                    print(name, pos)
+                # for name, pos in vicon.get_marker_positions("Wand").items():
+                #     print(name, pos)
 
                 print(vicon.get_subject_names())
                 for subject in vicon.get_subject_names():
                     for segment in vicon.get_segment_names(subject):
-                        print(subject, segment, vicon.get_segment_pose(subject, segment))
+                        # print(subject, segment, vicon.get_segment_pose(subject, segment))
+                        print(subject, segment, vicon.get_segment_local_rotation(subject, segment))
 
 
 if __name__ == "__main__":
