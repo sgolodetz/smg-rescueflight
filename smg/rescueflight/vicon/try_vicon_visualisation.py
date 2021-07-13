787b9687fded3a857800358c33dd2af108f9a2da
Stuart Golodetz <sgolodetz@gxstudios.net>
2021-07-13 16:04:24 +0100

    Make the visualiser use a mapping server.

--- smg/rescueflight/vicon/try_vicon_visualisation.py
+++ smg/rescueflight/vicon/try_vicon_visualisation.py
@@ -12,17 +12,16 @@ from OpenGL.GL import *
 from timeit import default_timer as timer
 from typing import Callable, Dict, Optional, Tuple
 
-from smg.imagesources import RGBFromRGBDImageSource, RGBImageSource
+from smg.comms.base import RGBDFrameMessageUtil, RGBDFrameReceiver
+from smg.comms.mapping import MappingServer
 from smg.meshing import MeshUtil
 from smg.opengl import CameraRenderer, OpenGLLightingContext, OpenGLMatrixContext, OpenGLTriMesh, OpenGLUtil
-from smg.openni import OpenNICamera, OpenNIRGBDImageSource
 from smg.rigging.cameras import SimpleCamera
 from smg.rigging.controllers import KeyboardCameraController
 from smg.rigging.helpers import CameraPoseConverter, CameraUtil
-from smg.rotory import DroneFactory, DroneRGBImageSource
 from smg.skeletons import Skeleton3D, SkeletonRenderer
 from smg.smplx import SMPLBody
-from smg.utility import FiducialUtil, GeometryUtil
+from smg.utility import FiducialUtil, GeometryUtil, PooledQueue
 from smg.vicon import LiveViconInterface, OfflineViconInterface, SubjectFromSourceCache
 from smg.vicon import ViconFrameSaver, ViconInterface, ViconSkeletonDetector
 
@@ -116,17 +115,16 @@ def main() -> None:
         help="the Vicon persistence mode"
     )
     parser.add_argument(
+        "--run_server", action="store_true",
+        help="whether to accept connections from mapping clients"
+    )
+    parser.add_argument(
         "--scenes_folder", type=str, default="C:/spaint/build/bin/apps/spaintgui/meshes",
         help="the folder from which to load the scene mesh"
     )
     parser.add_argument(
         "--scene_timestamp", type=str,
         help="a timestamp indicating which scene mesh to load"
-    )
-    parser.add_argument(
-        "--source_type", "-t", type=str, default="none",
-        choices=("anafi_thermal", "ardrone2", "kinect", "none", "tello"),
-        help="the image source type"
     )
     parser.add_argument(
         "--use_vicon_poses", action="store_true",
@@ -166,7 +164,7 @@ def main() -> None:
     subject_from_source_cache: SubjectFromSourceCache = SubjectFromSourceCache(".")
 
     # TODO: Comment here.
-    image_source: Optional[RGBImageSource] = None
+    mapping_server: Optional[MappingServer] = None
     vicon: Optional[ViconInterface] = None
 
     try:
@@ -176,22 +174,20 @@ def main() -> None:
         else:
             vicon = LiveViconInterface()
 
-        # If we're using an RGB image source, construct it.
-        source_type: str = args["source_type"]
-        if source_type == "kinect":
-            image_source = RGBFromRGBDImageSource(OpenNIRGBDImageSource(OpenNICamera(mirror_images=True)))
-        elif source_type != "none":
-            kwargs: Dict[str, dict] = {
-                "anafi_thermal": dict(),
-                "ardrone2": dict(print_commands=False, print_control_messages=False, print_navdata_messages=False),
-                "tello": dict(print_commands=False, print_responses=False, print_state_messages=False)
-            }
-            image_source = DroneRGBImageSource(DroneFactory.make_drone(source_type, **kwargs[source_type]))
+        # Run a mapping server if requested.
+        client_id: int = 0
+        receiver: RGBDFrameReceiver = RGBDFrameReceiver()
+        if args["run_server"]:
+            mapping_server = MappingServer(
+                frame_decompressor=RGBDFrameMessageUtil.decompress_frame_message,
+                pool_empty_strategy=PooledQueue.PES_REPLACE_RANDOM
+            )
+            mapping_server.start()
 
-        # If we're in output mode, construct the frame saver.
-        frame_saver: Optional[ViconFrameSaver] = None
+        # If we're in output mode, construct the Vicon frame saver.
+        vicon_frame_saver: Optional[ViconFrameSaver] = None
         if persistence_mode == "output":
-            frame_saver = ViconFrameSaver(folder=persistence_folder, vicon=vicon)
+            vicon_frame_saver = ViconFrameSaver(folder=persistence_folder, vicon=vicon)
 
         # Construct the skeleton detector.
         skeleton_detector: ViconSkeletonDetector = ViconSkeletonDetector(
@@ -229,7 +225,7 @@ def main() -> None:
             scene_mesh = load_scene_mesh(args["scenes_folder"], scene_timestamp, vicon)
 
         # Initialise the playback variables.
-        pause: bool = args["pause"]
+        pause: bool = args["pause"] or args["run_server"]
         previous_frame_number: Optional[int] = None
         previous_frame_start: Optional[float] = None
         process_next: bool = True
@@ -289,28 +285,44 @@ def main() -> None:
                         # Try to get a frame from the Vicon system. If that succeeds:
                         if vicon.get_frame():
                             frame_number: int = vicon.get_frame_number()
-                            frame_start: float = timer()
 
-                            # If we're in output mode, save the frame to disk.
-                            if persistence_mode == "output":
-                                frame_saver.save_frame()
+                            # If we're running a mapping server, try to get a frame from the client.
+                            colour_image: Optional[np.ndarray] = None
+                            if mapping_server is not None and mapping_server.has_frames_now(client_id):
+                                # # Get the camera parameters from the server.
+                                # height, width, _ = mapping_server.get_image_shapes(client_id)[0]
+                                # image_size = (width, height)
+                                # intrinsics = mapping_server.get_intrinsics(client_id)[0]
 
-                            # If we're also using an image source:
-                            if image_source is not None:
-                                # Get an image from the image source.
-                                image: np.ndarray = image_source.get_image()
+                                # Get the newest frame from the mapping server.
+                                mapping_server.peek_newest_frame(client_id, receiver)
+                                colour_image = receiver.get_rgb_image()
 
                                 # Show the image.
-                                cv2.imshow("Image", image)
+                                cv2.imshow("Received Image", colour_image)
                                 cv2.waitKey(1)
 
-                                # If we're in output mode, save the image to disk.
-                                if True:  # persistence_mode == "output":
+                            # If we're in output mode:
+                            if True:  # persistence_mode == "output":
+                                # If we aren't running a mapping server:
+                                if mapping_server is None:
+                                    # Save the Vicon frame to disk.
+                                    # vicon_frame_saver.save_frame()
+                                    print("Would save Vicon frame")
+
+                                # Otherwise, if we are running a server and an image has been obtained from the client:
+                                elif colour_image is not None:
+                                    # Save the Vicon frame to disk.
+                                    # vicon_frame_saver.save_frame()
+                                    print("Would save Vicon frame")
+
+                                    # Save the colour image to disk.
                                     filename: str = os.path.join(persistence_folder, f"{frame_number}.png")
                                     print(f"Would save image to {filename}")
 
                             # Check how long has elapsed since the start of the previous frame. If it's not long
                             # enough, pause until the expected amount of time has elapsed.
+                            frame_start: float = timer()
                             if previous_frame_number is not None:
                                 recording_fps: int = 200
                                 expected_time_delta: float = (frame_number - previous_frame_number) / recording_fps
@@ -403,9 +415,9 @@ def main() -> None:
             # Swap the front and back buffers.
             pygame.display.flip()
     finally:
-        # Terminate the image source.
-        if image_source is not None:
-            image_source.terminate()
+        # Terminate the mapping server.
+        if mapping_server is not None:
+            mapping_server.terminate()
 
         # Terminate the Vicon system.
         if vicon is not None:
