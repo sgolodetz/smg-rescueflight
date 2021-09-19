148f2cf9a4446ec205e9283021d9d8bf29759c8d
Stuart Golodetz <sgolodetz@gxstudios.net>
2021-09-19 16:20:05 +0100

    Add lots of comments.

--- smg/rescueflight/dvmvs/try_monocular_depth_estimator_offline.py
+++ smg/rescueflight/dvmvs/try_monocular_depth_estimator_offline.py
@@ -14,14 +14,14 @@ from smg.utility import GeometryUtil, ImageUtil
 
 
 def main() -> None:
-    depth_estimator: DVMVSMonocularDepthEstimator = DVMVSMonocularDepthEstimator()
-
+    # Determine the filenames of the images in the sequence, and load in the camera intrinsics and trajectory.
     scene_folder: Path = Path(Config.test_online_scene_path)
-
+    image_filenames: List[Path] = sorted((scene_folder / 'images').files("*.png"))
     intrinsics: np.ndarray = np.loadtxt(scene_folder / 'K.txt').astype(np.float32)
     poses: np.ndarray = np.fromfile(scene_folder / "poses.txt", dtype=float, sep="\n ").reshape((-1, 4, 4))
-    image_filenames: List[Path] = sorted((scene_folder / 'images').files("*.png"))
 
+    # Construct the depth estimator.
+    depth_estimator: DVMVSMonocularDepthEstimator = DVMVSMonocularDepthEstimator()
     depth_estimator.set_intrinsics(intrinsics)
 
     # Make the initial TSDF.
@@ -31,10 +31,17 @@ def main() -> None:
         color_type=o3d.pipelines.integration.TSDFVolumeColorType.RGB8
     )
 
+    # For each frame in the sequence:
     for i in tqdm(range(0, len(poses))):
+        # Read in the colour image.
         colour_image: np.ndarray = cv2.imread(image_filenames[i], cv2.IMREAD_COLOR)
+
+        # Try to estimate a corresponding depth image.
         estimated_depth_image: Optional[np.ndarray] = depth_estimator.estimate_depth(colour_image, poses[i])
+
+        # If this succeeds:
         if estimated_depth_image is not None:
+            # Show the colour and depth images.
             cv2.imshow("Colour Image", colour_image)
             cv2.imshow("Estimated Depth Image", estimated_depth_image / 5)
             cv2.waitKey(1)
@@ -50,6 +57,7 @@ def main() -> None:
                 o3d_intrinsics, tsdf
             )
 
+    # Visualise the reconstructed mesh.
     VisualisationUtil.visualise_geometry(ReconstructionUtil.make_mesh(tsdf))
 
 
