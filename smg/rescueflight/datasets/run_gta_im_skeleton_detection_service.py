c6f58fbceba2a8b83c8c40dd507ab2d24d4ecc82
Stuart Golodetz <sgolodetz@gxstudios.net>
2021-10-28 16:59:29 +0100

    More comments.

--- smg/rescueflight/datasets/run_gta_im_skeleton_detection_service.py
+++ smg/rescueflight/datasets/run_gta_im_skeleton_detection_service.py
@@ -13,18 +13,17 @@ from smg.comms.skeletons import SkeletonDetectionService
 from smg.skeletons import Keypoint, Skeleton3D
 
 
-# def generate_id_map(map_path):
-#     # FIXME: This function is currently borrowed from gta_utils.py in the GTA-IM code, but should really be imported.
-#     id_map = cv2.imread(map_path, -1)
-#     h, w, _ = id_map.shape
-#     id_map = np.concatenate(
-#         (id_map, np.zeros((h, w, 1), dtype=np.uint8)), axis=2
-#     )
-#     id_map.dtype = np.uint32
-#     return id_map
+def generate_id_map(map_path: str, image_size: Tuple[int, int]) -> np.ndarray:
+    """
+    Make an instance ID image of the specified size by loading one from disk and resizing it.
 
-def generate_id_map(map_path, image_size):
-    # FIXME: This function is adapted from gta_utils.py in the GTA-IM code.
+    .. note::
+        This function is adapted from gta_utils.py in the GTA-IM code.
+
+    :param map_path:    The path to the instance ID image on disk.
+    :param image_size:  The desired size of the output image.
+    :return:            The resized instance ID image.
+    """
     id_map = cv2.imread(map_path, -1)
     id_map = cv2.resize(id_map, image_size, interpolation=cv2.INTER_NEAREST)
     h, w, _ = id_map.shape
@@ -36,6 +35,20 @@ def generate_id_map(map_path, image_size):
 
 
 def make_keypoint(new_name: str, old_name: str, frame_info: Dict[str, Any], origin: np.ndarray) -> Tuple[str, Keypoint]:
+    """
+    Make a skeleton keypoint from its known position in the GTA-IM dataset.
+
+    .. note::
+        In practice, the origin will just be the initial position of the camera.
+
+    :param new_name:    The name we want to give the keypoint in our system.
+    :param old_name:    The name of the keypoint in the GTA-IM dataset.
+    :param frame_info:  The info for the frame (obtained from the GTA-IM .pickle file for the sequence).
+    :param origin:      The origin of the coordinate system in which we want to express the keypoint.
+    :return:            The keypoint.
+    """
+    # Transform the keypoint into the desired coordinate system. Note that the rotation here mirrors the rotation
+    # of the camera pose in try_load_frame in the GTA-IM client.
     position: np.ndarray = np.array(frame_info[old_name]) - origin
     position = np.array([
         [1, 0, 0],
@@ -46,8 +59,17 @@ def make_keypoint(new_name: str, old_name: str, frame_info: Dict[str, Any], orig
 
 
 def make_sequence_key(sequence_dir: str) -> str:
-    sequence_components: List[str] = os.path.normpath(sequence_dir).split(os.sep)
-    return f"{sequence_components[-2]}/{sequence_components[-1]}"
+    """
+    Make the key for a GTA-IM sequence based on the path to the sequence directory.
+
+    .. note::
+        For example, the sequence key for .../gta-im/FPS-5/2020-06-09-17-07-15 is FPS-5/2020-06-09-17-07-15.
+
+    :param sequence_dir:    The directory containing the GTA-IM sequence.
+    :return:                The key for the sequence.
+    """
+    components: List[str] = os.path.normpath(sequence_dir).split(os.sep)
+    return f"{components[-2]}/{components[-1]}"
 
 
 def make_frame_processor(sequence_dir: str, info: List[Dict[str, Any]], info_npz: np.lib.npyio.NpzFile) -> \
@@ -59,9 +81,9 @@ def make_frame_processor(sequence_dir: str, info: List[Dict[str, Any]], info_npz
     Make a frame processor for a skeleton detection service that yields ground-truths 3D skeletons and people masks
     loaded from a GTA-IM sequence on disk.
 
-    :param sequence_dir:    TODO
-    :param info:            TODO
-    :param info_npz:        TODO
+    :param sequence_dir:    The directory containing the GTA-IM sequence.
+    :param info:            The information loaded from the sequence's .pickle file.
+    :param info_npz:        The information loaded from the sequence's .npz file.
     :return:                The frame processor.
     """
     # noinspection PyUnusedLocal
@@ -71,15 +93,17 @@ def make_frame_processor(sequence_dir: str, info: List[Dict[str, Any]], info_npz
         """
         Load and yield the ground-truth 3D skeletons and people mask for the specified GTA-IM frame.
 
-        :param frame_idx:           TODO
+        :param frame_idx:           The frame index.
         :param colour_image:        Passed in by the skeleton detection service, but ignored.
         :param depth_image:         Passed in by the skeleton detection service, but ignored.
         :param world_from_camera:   Passed in by the skeleton detection service, but ignored.
         :param intrinsics:          Passed in by the skeleton detection service, but ignored.
         :return:                    The ground-truth 3D skeletons and people mask for the specified GTA-IM frame.
         """
+        # Get the frame info.
         frame_info: Dict[str, Any] = info[frame_idx]
 
+        # Construct the skeleton.
         world_from_initial: np.ndarray = np.linalg.inv(np.transpose(info_npz["world2cam_trans"][0]))
         origin: np.ndarray = world_from_initial[0:3, 3]
 
@@ -124,9 +148,12 @@ def make_frame_processor(sequence_dir: str, info: List[Dict[str, Any]], info_npz
 
         skeleton: Skeleton3D = Skeleton3D(keypoints, keypoint_pairs)
 
+        # Construct the people mask.
         height, width = colour_image.shape[:2]
         id_map: np.ndarray = generate_id_map(os.path.join(sequence_dir, f"{frame_idx:05d}_id.png"), (width, height))
 
+        # Note: The instance IDs corresponding to people don't seem to be available in the sequence information, so
+        #       I'm hard-coding them here for specific sequences. This will change when new sequences are added.
         sequence_to_person_ids: Dict[str, List[int]] = {
             "FPS-5/2020-06-09-17-14-03": [16386, 389890],
             "FPS-5/2020-06-10-21-53-42": [16386],
@@ -148,9 +175,6 @@ def make_frame_processor(sequence_dir: str, info: List[Dict[str, Any]], info_npz
         kernel_size: int = 5
         kernel: np.ndarray = np.ones((kernel_size, kernel_size), np.uint8)
         people_mask = cv2.dilate(people_mask, kernel)
-
-        # cv2.imshow("People Mask", people_mask)
-        # cv2.waitKey(1)
 
         return [skeleton], people_mask
 
@@ -184,14 +208,20 @@ def main() -> None:
     info: List[Dict[str, Any]] = pickle.load(open(os.path.join(sequence_dir, "info_frames.pickle"), "rb"))
     info_npz: np.lib.npyio.NpzFile = np.load(os.path.join(sequence_dir, "info_frames.npz"))
 
-    # TODO: Comment here.
+    # Check whether an optional frame index has been specified.
     frame_idx: Optional[int] = args.get("frame_idx")
+
+    # If it has:
     if frame_idx is not None:
-        id_map = generate_id_map(os.path.join(sequence_dir, f"{frame_idx:05d}_id.png"), (1920, 1080))
-        ids = np.unique(id_map)
-        print(ids)
+        # Generate the instance ID image for the frame.
+        id_map: np.ndarray = generate_id_map(os.path.join(sequence_dir, f"{frame_idx:05d}_id.png"), (1920, 1080))
+
+        # Show a binary mask for each unique instance ID, so that we can figure out which is which.
+        ids: np.ndarray = np.unique(id_map)
         for i in ids:
             cv2.imshow(f"ID {i}", cv2.resize(np.where(id_map == i, 255, 0).astype(np.uint8), (0, 0), fx=0.25, fy=0.25))
+
+        # If the user presses a key, early out.
         cv2.waitKey()
         exit(0)
 
