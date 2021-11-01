43c5c1741e6f6d5505af07dceec4a88a8ea51878
Stuart Golodetz <sgolodetz@gxstudios.net>
2021-11-01 20:56:03 +0000

    Minor changes.

--- smg/rescueflight/datasets/determine_scannet_root_dir.sh
+++ smg/rescueflight/datasets/determine_scannet_root_dir.sh
@@ -1,11 +1,13 @@
 #! /bin/bash
 
+# Check that the script is being used correctly.
 if [ $# -ne 0 ]
 then
   echo "Usage: determine_scannet_root_dir.sh"
   exit 1
 fi
 
+# Check various different possibilities for the ScanNet root directory, and echo the right one if found.
 root_dir="/c/datasets/scannet"
 if [ -e "$root_dir" ]
 then
--- smg/rescueflight/datasets/determine_scannet_sequence_dir.sh
+++ smg/rescueflight/datasets/determine_scannet_sequence_dir.sh
@@ -1,16 +1,23 @@
 #! /bin/bash
 
+# Check that the script is being used correctly.
 if [ $# -ne 1 ] && [ $# -ne 2 ]
 then
   echo "Usage: determine_scannet_sequence_dir.sh <scene name> [allow missing]"
   exit 1
 fi
 
+# Try to determine the location of the ScanNet root directory.
 root_dir=`./determine_scannet_root_dir.sh`
+
+# TODO: Check if this is the right condition when the root directory can't be found.
 if [ -e "$root_dir" ]
 then
+  # Derive the location of the sequence directory from the root directory.
   sequence_dir="$root_dir/$1"
-  if [ $# -eq 2 ] || [ -e "$sequence_dir" ]
+
+  # If the sequence directory either exists, or is allowed not to exist, echo it.
+  if [ -e "$sequence_dir" ] || [ $# -eq 2 ]
   then
     echo "$sequence_dir"
     exit 0
