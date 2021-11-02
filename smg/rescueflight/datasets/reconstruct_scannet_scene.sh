5c17237b8b7924e19aba0c7d31dd267d88018b9e
Stuart Golodetz <sgolodetz@gxstudios.net>
2021-11-02 14:56:06 +0000

    More comments.

--- smg/rescueflight/datasets/reconstruct_scannet_scene.sh
+++ smg/rescueflight/datasets/reconstruct_scannet_scene.sh
@@ -1,11 +1,13 @@
 #! /bin/bash -e
 
+# Check that the script is being used correctly.
 if [ $# -lt 4 ]
 then
-  echo "Usage: reconstruct_scannet_scene.sh <sequence name> <tag> {gt|dvmvs|mvdepth} {gt|track} [args]"
+  echo "Usage: reconstruct_scannet_scene.sh <sequence name> <method tag> {gt|dvmvs|mvdepth} {gt|track} [args]"
   exit 1
 fi
 
+# Check that the sequence directory exists.
 sequence_dir=`./determine_scannet_sequence_dir.sh "$1"`
 if [ -z "$sequence_dir" ]
 then
@@ -13,14 +15,17 @@ then
   exit 1
 fi
 
+# Start the reconstruction.
 echo "Reconstructing $1 ($2)"
 
+# If the output file already exists, early out.
 if [ -f "$sequence_dir/recon/$2.ply" ]
 then
   echo "- Found $2.ply: skipping"
   exit 0
 fi
 
+# Run the mapping server.
 CONDA_BASE=$(conda info --base)
 source "$CONDA_BASE\\etc\\profile.d\\conda.sh"
 conda activate smglib
@@ -33,8 +38,10 @@ else
   python ../mapping/scripts/run_open3d_mapping_server.py --batch --debug --output_dir="$sequence_dir/recon" -p wait --reconstruction_filename="$2.ply" --save_reconstruction --depth_estimator_type="$3" "${@:5}" > /dev/null 2>&1 &
 fi
 
+# Wait for the mapping server to initialise.
 sleep 5
 
+# Run the mapping client.
 echo "- Running mapping client..."
 if [ "$4" = "gt" ]
 then
@@ -43,5 +50,6 @@ else
   python run_scannet_client.py --batch -s "$sequence_dir" --use_tracker > /dev/null 2>&1
 fi
 
+# Wait for the reconstruction to be written to disk.
 echo "- Writing reconstruction to: $sequence_dir/recon/$2.ply"
 sleep 5
