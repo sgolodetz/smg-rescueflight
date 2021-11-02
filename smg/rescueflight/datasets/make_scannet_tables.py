e65cd55de0d21f14e4aff0584de519d6b8cec59b
Stuart Golodetz <sgolodetz@gxstudios.net>
2021-11-02 15:15:37 +0000

    Yet more tidying.

--- smg/rescueflight/datasets/make_scannet_tables.py
+++ smg/rescueflight/datasets/make_scannet_tables.py
@@ -14,17 +14,20 @@ def main() -> None:
     )
     args: dict = vars(parser.parse_args())
 
+    # Load in the names of the sequences we want to use.
     with open(args["sequence_list"]) as f:
         sequence_names: List[str] = [line.rstrip() for line in f.readlines()]
 
-    method_names: List[str] = [
+    # Specify the tags for the various different methods we want to consider.
+    method_tags: List[str] = [
         "dvmvs_4m_gt", "dvmvs_pp_4m_gt", "mvdepth_4m_gt", "mvdepth_pp_4m_gt"
     ]
 
+    # Load the results for all the sequences and methods into a big tensor.
     da_sequences: List[xr.DataArray] = []
     for sequence in sequence_names:
         da_methods: List[xr.DataArray] = []
-        for method in method_names:
+        for method in method_tags:
             filename: str = f"C:/datasets/scannet/{sequence}/recon/c2c_dist-{method}-gt_gt.txt"
             da_method: xr.DataArray = xr.DataArray(
                 pd.read_csv(filename, delimiter=' ').values[:, 0].astype(float),
@@ -32,17 +35,14 @@ def main() -> None:
             )
             da_methods.append(da_method)
 
-        da_sequence: xr.DataArray = xr.concat(da_methods, pd.Index(method_names, name="Method"))
+        da_sequence: xr.DataArray = xr.concat(da_methods, pd.Index(method_tags, name="Method"))
         da_sequence.name = sequence
         da_sequences.append(da_sequence)
 
     da: xr.DataArray = xr.concat(da_sequences, pd.Index(sequence_names, name="Sequence"))
     da.name = "Data"
 
-    # print(da)
-    # print(da.sel(Attribute="median").idxmin(dim="Method"))
-    # print(da.sel(Attribute="mean"))
-    # print(da.sel(Attribute="mean").mean(dim="Sequence").to_pandas())
+    # Print out the tables we want to use for the paper.
     print(da.sel(Attribute="mean").to_pandas())
     print()
     print(da.sel(Attribute=["mean", "median", "std"]).mean(dim="Sequence").to_pandas())
