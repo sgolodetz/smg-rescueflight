e3735cde418bc9c3ae18a2d7675206d08ea289ca
Stuart Golodetz <sgolodetz@gxstudios.net>
2021-11-02 14:43:34 +0000

    More tidying.

--- smg/rescueflight/datasets/evaluate_scannet_sequence.sh
+++ smg/rescueflight/datasets/evaluate_scannet_sequence.sh
@@ -1,14 +1,17 @@
 #! /bin/bash -e
 
+# Check that the script is being used correctly.
 if [ $# -ne 1 ]
 then
   echo "Usage: evaluate_scannet_sequence.sh <sequence name>"
   exit 1
 fi
 
+# Repeatedly reconstruct the ScanNet sequence using the various different methods we want to compare.
 ./reconstruct_scannet_sequence.sh "$1"
 
-for tag in dvmvs_4m_gt dvmvs_pp_4m_gt mvdepth_4m_gt mvdepth_pp_4m_gt
+# Evaluate the reconstructions for each of the different methods in turn.
+for method_tag in dvmvs_4m_gt dvmvs_pp_4m_gt mvdepth_4m_gt mvdepth_pp_4m_gt
 do
-  ./evaluate_scannet_scene.sh "$1" "$tag" gt_gt
+  ./evaluate_scannet_scene.sh "$1" "$method_tag" gt_gt
 done
--- smg/rescueflight/datasets/evaluate_scannet_sequences.sh
+++ smg/rescueflight/datasets/evaluate_scannet_sequences.sh
@@ -1,16 +1,19 @@
 #! /bin/bash -e
 
+# Check that the script is being used correctly.
 if [ $# -ne 1 ]
 then
-  echo "Usage: evaluate_scannet_sequences.sh <sequence list>"
+  echo "Usage: evaluate_scannet_sequences.sh <sequence list file>"
   exit 1
 fi
 
-tr -d '\r' < "$1" | while read f
+# Read a list of ScanNet sequences from a file, and run the evaluation for each one in turn.
+tr -d '\r' < "$1" | while read sequence_name
 do
-  ./evaluate_scannet_sequence.sh "$f"
+  ./evaluate_scannet_sequence.sh "$sequence_name"
 done
 
+# Make the output tables for the paper.
 CONDA_BASE=$(conda info --base)
 source "$CONDA_BASE\\etc\\profile.d\\conda.sh"
 conda activate smglib
