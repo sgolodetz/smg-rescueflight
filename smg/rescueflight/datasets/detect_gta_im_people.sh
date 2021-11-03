700f4a7f95dd5c63b86ed7aa89d61d9e2b51b73f
Stuart Golodetz <sgolodetz@gxstudios.net>
2021-11-03 17:05:01 +0000

    Automatically determine the output directory.

--- smg/rescueflight/datasets/detect_gta_im_people.sh
+++ smg/rescueflight/datasets/detect_gta_im_people.sh
@@ -3,7 +3,7 @@
 # Check that the script is being used correctly.
 if [ $# -lt 4 ]
 then
-  echo "Usage: detect_gta_im_skeletons.sh <sequence name> <method tag> {gt|lcrnet|maskrcnn} {gt|lcrnet|xnect} [args]"
+  echo "Usage: detect_gta_im_people.sh <sequence name> <method tag> {gt|lcrnet|maskrcnn} {gt|lcrnet|xnect} [args]"
   exit 1
 fi
 
@@ -19,13 +19,13 @@ fi
 CONDA_BASE=$(conda info --base)
 source "$CONDA_BASE\\etc\\profile.d\\conda.sh"
 
-# Start the skeleton detection process.
-echo "Detecting skeletons for $1 ($2)"
+# Start the people detection process.
+echo "Detecting people for $1 ($2)"
 
 # If the output directory already exists, early out.
-if [ -e "$sequence_dir/skeletons/$2" ]
+if [ -e "$sequence_dir/people/$2" ]
 then
-  echo "- Found skeletons/$2 directory: skipping"
+  echo "- Found people/$2 directory: skipping"
   exit 0
 fi
 
@@ -57,7 +57,7 @@ sleep 5
 # Run the mapping server.
 echo "- Running mapping server..."
 conda activate smglib
-python ../mapping/scripts/run_octomap_mapping_server.py --detect_skeletons -p wait --use_received_depth --octree_voxel_size=0.1 "${@:5}" > /dev/null 2>& 1 &
+python ../mapping/scripts/run_octomap_mapping_server.py --detect_skeletons -p wait --use_received_depth --octree_voxel_size=0.1 --output_dir="$sequence_dir/people/$2" "${@:5}" > /dev/null 2>& 1 &
 conda deactivate
 
 # Wait for the mapping server to initialise.
