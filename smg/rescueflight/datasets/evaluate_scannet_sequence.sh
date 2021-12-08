f07987a2e9570c8efc5a8b5e12cda66798ce0bb0
Stuart Golodetz <sgolodetz@gxstudios.net>
2021-12-08 21:28:55 +0000

    Various changes.

--- smg/rescueflight/datasets/evaluate_scannet_sequence.sh
+++ smg/rescueflight/datasets/evaluate_scannet_sequence.sh
@@ -11,7 +11,7 @@ fi
 ./reconstruct_scannet_sequence.sh "$1"
 
 # Evaluate the reconstructions for each of the different methods in turn.
-for method_tag in dvmvs_4m_gt dvmvs_pp_4m_gt mvdepth_4m_gt mvdepth_pp_4m_gt mvdepth2_4m_gt mvdepth2_pp_4m_gt
+for method_tag in dvmvs_4m_gt dvmvs_pp_4m_gt mvdepth_4m_gt mvdepth_pp_4m_gt  # mvdepth2_4m_gt mvdepth2_pp_4m_gt
 do
   ./evaluate_scene.sh scannet "$1" "$method_tag" gt_gt
   ./evaluate_scene.sh scannet "$1" gt_gt "$method_tag"
--- smg/rescueflight/datasets/reconstruct_scannet_sequence.sh
+++ smg/rescueflight/datasets/reconstruct_scannet_sequence.sh
@@ -11,10 +11,10 @@ fi
 ./obtain_scannet_sequence.sh "$1"
 
 # Reconstruct the sequence using the various different methods we want to compare.
-./reconstruct_scannet_scene.sh "$1" "gt_gt" gt gt --max_depth=4.0
-./reconstruct_scannet_scene.sh "$1" "dvmvs_4m_gt" dvmvs gt --max_depth=4.0 --no_depth_postprocessing
-./reconstruct_scannet_scene.sh "$1" "dvmvs_pp_4m_gt" dvmvs gt --max_depth=4.0
-./reconstruct_scannet_scene.sh "$1" "mvdepth_4m_gt" mvdepth gt --max_depth=4.0 --no_depth_postprocessing
-./reconstruct_scannet_scene.sh "$1" "mvdepth_pp_4m_gt" mvdepth gt --max_depth=4.0
-./reconstruct_scannet_scene.sh "$1" "mvdepth2_4m_gt" mvdepth2 gt --max_depth=4.0 --no_depth_postprocessing
-./reconstruct_scannet_scene.sh "$1" "mvdepth2_pp_4m_gt" mvdepth2 gt --max_depth=4.0
+./reconstruct_scannet_scene.sh "$1" "gt_gt" gt gt --max_depth=4.0 --voxel_size=0.025
+./reconstruct_scannet_scene.sh "$1" "dvmvs_4m_gt" dvmvs gt --max_depth=4.0 --no_depth_postprocessing --voxel_size=0.025
+./reconstruct_scannet_scene.sh "$1" "dvmvs_pp_4m_gt" dvmvs gt --max_depth=4.0 --voxel_size=0.025
+./reconstruct_scannet_scene.sh "$1" "mvdepth_4m_gt" mvdepth gt --max_depth=4.0 --no_depth_postprocessing --voxel_size=0.025
+./reconstruct_scannet_scene.sh "$1" "mvdepth_pp_4m_gt" mvdepth gt --max_depth=4.0 --voxel_size=0.025
+#./reconstruct_scannet_scene.sh "$1" "mvdepth2_4m_gt" mvdepth2 gt --max_depth=4.0 --no_depth_postprocessing --voxel_size=0.025
+#./reconstruct_scannet_scene.sh "$1" "mvdepth2_pp_4m_gt" mvdepth2 gt --max_depth=4.0 --voxel_size=0.025
