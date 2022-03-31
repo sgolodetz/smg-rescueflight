29051d858c8caa62d282818c88dab5cbef672e3a
Stuart Golodetz <sgolodetz@gxstudios.net>
2022-03-31 14:50:31 +0100

    Modify the Futaba T6K test code to use the drone controller.

--- smg/rescueflight/rotory/examples/try_drone_futaba_t6k.py
+++ smg/rescueflight/rotory/examples/try_drone_futaba_t6k.py
@@ -1,21 +1,23 @@
 import cv2
+import numpy as np
 import os
 
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 import pygame
 
 from argparse import ArgumentParser
-from typing import Dict
+from typing import Dict, List
 
 from smg.joysticks import FutabaT6K
 from smg.rotory import DroneFactory
+from smg.rotory.controllers import DroneController, FutabaT6KDroneController
 
 
 def main():
     # Parse any command-line arguments.
     parser = ArgumentParser()
     parser.add_argument(
-        "--drone_type", "-t", type=str, required=True, choices=("ardrone2", "tello"),
+        "--drone_type", "-t", type=str, required=True, choices=("ardrone2", "simulated", "tello"),
         help="the drone type"
     )
     args: dict = vars(parser.parse_args())
@@ -37,41 +39,39 @@ def main():
     joystick = FutabaT6K(joystick_idx)
     joystick.calibrate()
 
-    # Use the Futaba T6K to control a drone.
+    # Construct the drone.
     kwargs: Dict[str, dict] = {
         "ardrone2": dict(print_commands=True, print_control_messages=True, print_navdata_messages=False),
+        "simulated": dict(),
         "tello": dict(print_commands=True, print_responses=True, print_state_messages=False)
     }
 
     drone_type: str = args.get("drone_type")
 
     with DroneFactory.make_drone(drone_type, **kwargs[drone_type]) as drone:
-        # Stop when both Button 0 and Button 1 on the Futaba T6K are set to their "released" state.
-        while joystick.get_button(0) != 0 or joystick.get_button(1) != 0:
+        # Construct the drone controller.
+        drone_controller: DroneController = FutabaT6KDroneController(drone, joystick)
+
+        # Stop when the drone controller asks us to quit.
+        while not drone_controller.should_quit():
+            # Record any PyGame events for later use by the drone controller.
+            events: List[pygame.event.Event] = []
             for event in pygame.event.get():
-                if event.type == pygame.JOYBUTTONDOWN:
-                    # If Button 0 on the Futaba T6K is set to its "pressed" state, take off.
-                    if event.button == 0:
-                        drone.takeoff()
-                elif event.type == pygame.JOYBUTTONUP:
-                    # If Button 0 on the Futaba T6K is set to its "released" state, land.
-                    if event.button == 0:
-                        drone.land()
-
-            # Update the movement of the drone based on the pitch, roll and yaw values output by the Futaba T6K.
-            drone.move_forward(joystick.get_pitch())
-            drone.turn(joystick.get_yaw())
-
-            if joystick.get_button(1) == 0:
-                drone.move_right(0)
-                drone.move_up(joystick.get_roll())
-            else:
-                drone.move_right(joystick.get_roll())
-                drone.move_up(0)
+                events.append(event)
 
             # Get the most recent image from the drone and show it.
-            cv2.imshow("Image", drone.get_image())
+            drone_image: np.ndarray = drone.get_image()
+            cv2.imshow("Image", drone_image)
             cv2.waitKey(1)
+
+            # Allow the user to control the drone.
+            drone_controller.update(events=events, image=drone_image, intrinsics=drone.get_intrinsics())
+
+            # FIXME: Delete these before merging.
+            # from smg.rotory.drones import SimulatedDrone
+            # sim_drone: SimulatedDrone = drone
+            # _, drone_w_t_c, _ = sim_drone.get_image_and_poses()
+            # print(drone_w_t_c)
 
     # Shut down pygame cleanly.
     pygame.quit()
