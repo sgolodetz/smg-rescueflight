23c881c0d4c9a4ae1f4b64ca5306b475eafdc87a
Stuart Golodetz <sgolodetz@gxstudios.net>
2022-03-31 14:38:06 +0100

    Allow the drone simulator to be controlled using the keyboard.

--- smg/rescueflight/rotorsim/run_drone_simulator.py
+++ smg/rescueflight/rotorsim/run_drone_simulator.py
@@ -9,6 +9,10 @@ def main() -> None:
     # Parse any command-line arguments.
     parser = ArgumentParser()
     parser.add_argument(
+        "--drone_controller_type", type=str, default="keyboard", choices=("futabat6k", "keyboard"),
+        help="the type of drone controller to use"
+    )
+    parser.add_argument(
         "--planning_octree", type=str,
         help="the name of the planning octree file"
     )
@@ -28,6 +32,7 @@ def main() -> None:
     # Construct the drone simulator.
     with DroneSimulator(
         debug=False,
+        drone_controller_type=args.get("drone_controller_type"),
         drone_mesh=MeshUtil.load_tello_mesh(),
         intrinsics=intrinsics,
         plan_paths=True,
