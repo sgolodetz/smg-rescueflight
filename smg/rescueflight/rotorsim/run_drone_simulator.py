f19c3a68f3fb285f60aa7803c53468ae3e54e3e2
Stuart Golodetz <sgolodetz@gxstudios.net>
2022-04-20 13:57:15 +0100

    Allow the drone simulator to use RTS-style drone control.

--- smg/rescueflight/rotorsim/run_drone_simulator.py
+++ smg/rescueflight/rotorsim/run_drone_simulator.py
@@ -9,7 +9,7 @@ def main() -> None:
     # Parse any command-line arguments.
     parser = ArgumentParser()
     parser.add_argument(
-        "--drone_controller_type", type=str, default="keyboard", choices=("futaba_t6k", "keyboard"),
+        "--drone_controller_type", "-t", type=str, default="keyboard", choices=("futaba_t6k", "keyboard", "rts"),
         help="the type of drone controller to use"
     )
     parser.add_argument(
@@ -35,7 +35,6 @@ def main() -> None:
         drone_controller_type=args.get("drone_controller_type"),
         drone_mesh=MeshUtil.load_tello_mesh(),
         intrinsics=intrinsics,
-        plan_paths=True,
         planning_octree_filename=args.get("planning_octree"),
         scene_mesh_filename=args.get("scene_mesh"),
         scene_octree_filename=args.get("scene_octree")
