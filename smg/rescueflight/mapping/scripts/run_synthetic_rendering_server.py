220b22d2da7aa56f70aaab7b2f475a079b2dd862
Stuart Golodetz <sgolodetz@gxstudios.net>
2022-05-13 11:56:56 +0100

    Continue working on the synthetic rendering server.

--- smg/rescueflight/mapping/scripts/run_synthetic_rendering_server.py
+++ smg/rescueflight/mapping/scripts/run_synthetic_rendering_server.py
@@ -18,7 +18,7 @@ from smg.opengl import CameraRenderer, OpenGLMatrixContext, OpenGLTriMesh, OpenG
 from smg.rigging.cameras import SimpleCamera
 from smg.rigging.controllers import KeyboardCameraController
 from smg.rigging.helpers import CameraPoseConverter, CameraUtil
-from smg.utility import GeometryUtil, PooledQueue
+from smg.utility import CameraParameters, GeometryUtil, PooledQueue
 
 
 def main() -> None:
@@ -27,16 +27,29 @@ def main() -> None:
     # Parse any command-line arguments.
     parser = ArgumentParser()
     parser.add_argument(
+        "--calibration_filename", type=str, required=True,
+        help="the name of the file containing the camera calibration parameters"
+    )
+    parser.add_argument(
         "--scene_mesh_filename", type=str, required=True,
         help="the name of the file containing the scene mesh"
     )
     args: dict = vars(parser.parse_args())
 
+    calibration_filename: str = args["calibration_filename"]
     scene_mesh_filename: str = args["scene_mesh_filename"]
+
+    # Load in the camera calibration parameters.
+    calib: Optional[CameraParameters] = CameraParameters.try_load(calibration_filename)
+    if calib is None:
+        raise RuntimeError("Error: Could not load camera parameters")
+
+    image_size: Tuple[int, int] = calib.get_image_size("colour")
+    intrinsics: Optional[Tuple[float, float, float, float]] = calib.get_intrinsics("colour")
 
     # Initialise PyGame and create the window.
     pygame.init()
-    window_size: Tuple[int, int] = (640, 480)
+    window_size: Tuple[int, int] = image_size
     pygame.display.set_mode(window_size, pygame.DOUBLEBUF | pygame.OPENGL)
     pygame.display.set_caption("Synthetic Rendering Server")
 
@@ -54,6 +67,7 @@ def main() -> None:
     )
 
     # Load in the scene mesh.
+    # noinspection PyUnusedLocal
     scene_mesh: Optional[OpenGLTriMesh] = None
     if os.path.exists(scene_mesh_filename):
         # noinspection PyUnresolvedReferences
@@ -66,8 +80,6 @@ def main() -> None:
         pool_empty_strategy=PooledQueue.PES_REPLACE_RANDOM
     ) as server:
         client_id: int = 0
-        image_size: Optional[Tuple[int, int]] = None
-        intrinsics: Optional[Tuple[float, float, float, float]] = None
         receiver: RGBDFrameReceiver = RGBDFrameReceiver()
         tracker_w_t_c: Optional[np.ndarray] = None
 
@@ -89,11 +101,6 @@ def main() -> None:
 
             # If the server has a frame from the client that has not yet been processed:
             if server.has_frames_now(client_id):
-                # Get the camera parameters from the server.
-                height, width, _ = server.get_image_shapes(client_id)[0]
-                image_size = (width, height)
-                intrinsics = server.get_intrinsics(client_id)[0]
-
                 # Get the pose of the newest frame from the server.
                 server.peek_newest_frame(client_id, receiver)
                 tracker_w_t_c = receiver.get_pose()
@@ -106,21 +113,21 @@ def main() -> None:
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
             # Once at least one frame has been received:
-            if image_size is not None:
+            if tracker_w_t_c is not None:
                 # Set the projection matrix.
                 with OpenGLMatrixContext(GL_PROJECTION, lambda: OpenGLUtil.set_projection_matrix(
-                    GeometryUtil.rescale_intrinsics(intrinsics, image_size, window_size), *window_size
+                    intrinsics, *window_size
                 )):
                     # Set the model-view matrix.
                     with OpenGLMatrixContext(GL_MODELVIEW, lambda: OpenGLUtil.load_matrix(
                         CameraPoseConverter.pose_to_modelview(np.linalg.inv(tracker_w_t_c))
                     )):
-                        # Render a voxel grid.
-                        glColor3f(0.0, 0.0, 0.0)
-                        OpenGLUtil.render_voxel_grid([-2, -2, -2], [2, 0, 2], [1, 1, 1], dotted=True)
-
-                        # Render coordinate axes.
-                        CameraRenderer.render_camera(CameraUtil.make_default_camera())
+                        # # Render a voxel grid.
+                        # glColor3f(0.0, 0.0, 0.0)
+                        # OpenGLUtil.render_voxel_grid([-2, -2, -2], [2, 0, 2], [1, 1, 1], dotted=True)
+                        #
+                        # # Render coordinate axes.
+                        # CameraRenderer.render_camera(CameraUtil.make_default_camera())
 
                         # Render the scene mesh.
                         if scene_mesh is not None:
@@ -128,6 +135,13 @@ def main() -> None:
 
             # Swap the front and back buffers.
             pygame.display.flip()
+
+            colour_image: np.ndarray = OpenGLUtil.read_bgr_image(*image_size)
+            depth_image: np.ndarray = OpenGLUtil.read_depth_image(*image_size)
+            import cv2
+            cv2.imshow("Colour Image", colour_image)
+            cv2.imshow("Depth Image", depth_image / 5)
+            cv2.waitKey(1)
 
 
 if __name__ == "__main__":
