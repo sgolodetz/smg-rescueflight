c891680d64a0f95bfb18fc9789ef79edaf5a63d6
Stuart Golodetz <sgolodetz@gxstudios.net>
2022-05-24 14:56:14 +0100

    Further tidying.

--- smg/rescueflight/relocalisation/try_dsacstar_relocaliser.py
+++ smg/rescueflight/relocalisation/try_dsacstar_relocaliser.py
@@ -14,39 +14,46 @@ def main() -> None:
     parser = ArgumentParser()
     parser.add_argument(
         "--hypothesis_count", type=int, default=64,
-        help="TODO"
+        help="the number of RANSAC hypotheses to consider"
+    )
+    parser.add_argument(
+        "--image_height", type=int, default=480,
+        help="the height to which the colour images will be rescaled"
     )
     parser.add_argument(
         "--inlier_alpha", type=float, default=100.0,
-        help="TODO"
+        help="the alpha parameter to use for soft inlier counting"
     )
     parser.add_argument(
         "--inlier_threshold", type=float, default=10.0,
-        help="TODO"
+        help="the inlier threshold to use when sampling RANSAC hypotheses (in pixels)"
     )
     parser.add_argument(
         "--max_pixel_error", type=float, default=100.0,
-        help="TODO"
+        help="the maximum reprojection error to use when checking pose consistency (in pixels)"
     )
     parser.add_argument(
         "--network_filename", type=str, required=True,
-        help="TODO"
+        help="the name of the file containing the DSAC* network"
     )
     parser.add_argument(
         "--tiny", action="store_true",
-        help="TODO"
+        help="whether to load a tiny network to massively reduce the memory footprint"
     )
     args: dict = vars(parser.parse_args())
 
+    # Construct the relocaliser.
     relocaliser: DSACStarRelocaliser = DSACStarRelocaliser(
+        args["network_filename"],
         hypothesis_count=args["hypothesis_count"],
+        image_height=args["image_height"],
         inlier_alpha=args["inlier_alpha"],
         inlier_threshold=args["inlier_threshold"],
         max_pixel_error=args["max_pixel_error"],
-        network_filename=args["network_filename"],
         tiny=args["tiny"]
     )
 
+    # TODO: Comment here.
     image: np.ndarray = cv2.imread("C:/smglib/smg-relocalisation/smg/external/dsacstar/datasets/7scenes_heads/test/rgb/seq-01-frame-000000.color.png")
     print(relocaliser.estimate_pose(image, 525.0))
     print(PoseUtil.load_pose("C:/smglib/smg-relocalisation/smg/external/dsacstar/datasets/7scenes_heads/test/poses/seq-01-frame-000000.pose.txt"))
